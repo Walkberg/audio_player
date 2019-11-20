4f6a37d2a47997d6f07d6e110210dac6be10293c
Samuel Bergerot <jeremy.roquin@dpmo.io>
2019-11-20 10:22:42 +0100

    * add detail

--- src/view/audioPlayer.js
+++ src/view/audioPlayer.js
@@ -8,7 +8,13 @@ import PlayArrow from "@material-ui/icons/PlayArrowRounded";
 import Pause from "@material-ui/icons/PauseRounded";
 import Avatar from "@material-ui/core/Avatar";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import Typography from '@material-ui/core/Typography';
+import {fadeInUp} from "react-animations" 
+import styled, { keyframes } from 'styled-components';
 
+
+
+//const Bounce =styled.div`animation: 2s ${keyframes`${fadeInUp}`} infinite`
 const BorderLinearProgress = withStyles({
   root: {
     height: 5,
@@ -31,31 +37,47 @@ const useStyles = makeStyles(theme => ({
   },
   paper: {
     // padding: theme.spacing(3)
+    zIndex:"501",
+    borderRadius:"15px",
     flexDirection: "row",
     display: "flex",
-    padding: "15px",
+    padding: "10px",
     boxShadow:
-      "0px 8px 9px -5px rgba(189,140,153,0.2),0px 15px 22px 2px rgba(189,140,153,0.2),0px 6px 28px 5px rgba(189,140,153,0.2)"
+      "0px 35px 9px -5px rgba(189,140,153,0.2),0px 15px 22px 2px rgba(189,140,153,0.2),0px 6px 28px 5px rgba(189,140,153,0.2)"
   },
   bigAvatar: {
     position: "absolute",
-    top: -75,
-    left: 0,
-    margin: 10,
     width: 125,
     height: 125,
     zIndex: 500
   },
-  rotation: {
-    animation: "App-logo-spin infinite 20s linear"
+  avatar:{
+    top: -75,
+    left: 0,
+    margin: 10,
   },
-
+  rotation: {
+    animation: "App-logo-spin infinite 20s linear",
+  },
+  shadow:{
+    boxShadow: "0px 5px 10px 5px rgb(0,0,0,0.2)"
+  },
   button: {
     borderRadius: "15%"
   },
   paper2: {
+    zIndex:"500",
+    position:"relative",
+    display: "flex",
     width: "90%",
-    backgroundColor:"#fff7"
+    backgroundColor: "#fff7",
+
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column"
   }
 }));
 
@@ -65,24 +87,46 @@ export default function AudioPlayer() {
 
   return (
     <main className={classes.root}>
-      <div style ={{justifyContent:"center"}}>
+      <div className={classes.container}>
         <Paper className={classes.paper2}>
-          <h5>title</h5>
-          <h6>artist</h6>
-          <BorderLinearProgress
-            variant="determinate"
-            color="secondary"
-            value={5}
-          />
+          <div style={{ position: "relative", width: "150px"  }}></div>
+          <div
+            style={{
+              bottom:"-10px",
+              display: "flex",
+              flexDirection: "column",
+              flexGrow: 1,
+              padding: "15px",
+              flexShrink:1,
+              justifyContent:"flex-start"
+            }}
+          >
+            <Typography variant="body2" gutterBottom>title</Typography>
+            <Typography  variant="body2" gutterBottom>artist</Typography>
+            <BorderLinearProgress
+              variant="determinate"
+              color="secondary"
+              value={5}
+            />
+          </div>
         </Paper>
         <Paper elevation={15} className={classes.paper}>
-          <div style={{ position: "relative", width: "150px" }}>
-            <Avatar
-              src="http://www.idolesmag.com/images/photos2017/8232-slimane-pochette-album-solune-26-janvier-2018.jpg"
-              className={[classes.bigAvatar, isPlaying && classes.rotation]}
-            >
-              <Avatar style={{ backgroundColor: "green", zIndex: 501 }} />
-            </Avatar>
+          <div style={{position:"relative", width: "150px" }}>
+            <Avatar  className={[classes.bigAvatar, classes.avatar, isPlaying && classes.shadow]}>
+              <Avatar
+                src="http://www.idolesmag.com/images/photos2017/8232-slimane-pochette-album-solune-26-janvier-2018.jpg"
+                className={[classes.bigAvatar,  isPlaying && classes.rotation]}
+              ></Avatar>
+              <Avatar
+                style={{
+                  display: "absolute",
+                  backgroundColor: "#fff",
+                  height:30,
+                  width:30,
+                  zIndex: 501
+                }}
+              />
+              </Avatar>
           </div>
           <IconButton aria-label="delete" className={classes.button}>
             <FastRewind fontSize="large" />
